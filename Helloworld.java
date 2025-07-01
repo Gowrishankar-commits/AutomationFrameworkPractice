3ce4a34d064c04c03a29725f87d7dbe560aa6ddb
Gowrishankar-commits <gowrishankar.m23@gmail.com>
2025-07-01 23:40:48 +0530

    Saving work on Helloworld

--- Helloworld.java
+++ Helloworld.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello Java");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		String revers ="";
+		
+		for (int i = name.length()-1; i>=0; i--) {
+			revers +=name.charAt(i);
+		}
+		System.out.println(revers);
 	}
-
+				
 }
