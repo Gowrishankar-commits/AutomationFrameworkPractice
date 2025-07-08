581fffe09a044b2eac225434d5ea1489b2e94833
Gowrishankar-commits <gowrishankar.m23@gmail.com>
2025-07-08 09:40:36 +0530

    Day2 push loop

---
+++ Day2_LoopsPractice.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Day2_LoopsPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+/*
+		  int x = sc.nextInt();
+		
+		if(x%2==0) {
+			System.out.println("Even");
+		}
+		else{
+			System.out.println("Odd");
+		}
+		
+		System.out.println("Enter your age : ");
+		int age = sc.nextInt();
+		if(age>=18) {
+			System.out.println("Eligible to vote for this election");
+		}
+		else {
+			System.out.println(" Not eligible to vote in election ");
+		}
+		//System.out.println("Enter number for multiplication");
+		//int two = sc.nextInt();
+		for(int b = 0; b<=20; b++) {
+			if(b%2==0) {
+				System.out.println(b+" number is even");
+			}
+
+		}
+
+		}
+
+	
+	int sum = 0;
+	for (int i =1; i<=10; i++ ) {
+	sum +=i;
+	}
+System.out.println("the first naturan numbers for 10 is "+sum);
+ 
+
+		int  q =  sc.nextInt();
+		for(int w =0; w<=10; w++) {
+		System.out.println(q + "*" + w + "=" + q*w);
+		}
+		
+		int r = sc.nextInt();
+		int e = 1;
+		while(e <=10) {
+			System.out.println(r + "*" + e + "=" + r*e);
+			e++;
+		}
+	
+		int t = sc.nextInt();
+		int y =1;
+		do {
+		System.out.println(t + "*"+ " y"+" = "+ "");
+		y++;
+		}
+		while(t<=10);
+		
+	*/
+
+}
+}
